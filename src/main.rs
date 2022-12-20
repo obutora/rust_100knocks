716b32114afcc3a188a90f9f6cf0f603af3e7f09
obutora <obutora@gmail.com>
2022-12-20 17:34:58 +0900

    add: a91

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,45 @@ use polars::prelude::*;
 fn main() {
     let recept_path = "100knocks-preprocess/docker/work/data/receipt.csv";
     // let store_path = "100knocks-preprocess/docker/work/data/store.csv";
-    // let customer_path = "100knocks-preprocess/docker/work/data/customer.csv";
+    let customer_path = "100knocks-preprocess/docker/work/data/customer.csv";
     // let product_path = "100knocks-preprocess/docker/work/data/product.csv";
     // let category_path = "100knocks-preprocess/docker/work/data/category.csv";
     // let geocode_path = "100knocks-preprocess/docker/work/data/geocode.csv";
 
-    fn ymd_to_ym(ymd: &Series) -> Series {
-        ymd.i64()
-            .unwrap()
-            .into_iter()
-            .map(|ymd| match ymd {
-                Some(ymd) => (ymd as f64 / 100.0).floor() as i32,
-                None => 0i32,
-            })
-            .collect()
-    }
-
+    let customer_df = LazyCsvReader::new(customer_path)
+    .has_header(true)
+    .finish()
+    .unwrap();
 
     let recept_df = LazyCsvReader::new(recept_path)
     .has_header(true)
     .finish()
     .unwrap()
+    .groupby([col("customer_id")])
+    .agg([col("amount").sum().alias("amount")]);
+
+    let joined = customer_df.left_join(recept_df, col("customer_id"), col("customer_id"))
     .select([
-        all(),
-        col("sales_ymd").map(|s| Ok(ymd_to_ym(&s)), GetOutput::default()).alias("sales_ym")
-    ])
-    .groupby([col("sales_ym")])
-    .agg([col("amount").sum().alias("amount")])
-    .sort(
-        "sales_ym",
-        SortOptions {
-            descending: (false),
-            nulls_last: (true),
-        },
-    )
-    .collect()
-    .unwrap();
+        col("*").exclude(["amount"]),
+        col("amount").fill_null(lit(0)).alias("amount")
+    ]);
 
-    let train1 = recept_df.clone().slice(0, 12);
-    let val1 = recept_df.clone().slice(12, 6);
+    let non_zero_df = joined.clone().filter(col("amount").gt(0))
+    .collect().unwrap();
 
-    let train2 = recept_df.slice(6, 12);
-    let val2 = recept_df.slice(18, 6);
+    let zero_df = joined.filter(col("amount").eq(0))
+    .collect().unwrap();
 
-    let train3 = recept_df.slice(12, 12);
-    let val3 = recept_df.slice(24, 6);
+    println!("non-zero :{:?}", non_zero_df.shape()); //zero_dfよりすくないので、こっちに合わせる
+    println!("zero : {:?}", zero_df.shape());
 
-    println!("{:?}", train3);
-    println!("{:?}", val3);
+    let zero_df = zero_df
+        .sample_n(non_zero_df.shape().0, false, true, None).unwrap();
+
+    println!("picked zero : {:?}", zero_df.shape());
+
+    
+    // println!("{:?}", joined.collect().unwrap());
     
 }
 
@@ -133,3 +124,39 @@ fn main() {
 
 // println!("{:?}", train3);
 // println!("{:?}", val3);
+
+
+
+
+//NOTE : A91
+//let customer_df = LazyCsvReader::new(customer_path)
+// .has_header(true)
+// .finish()
+// .unwrap();
+
+// let recept_df = LazyCsvReader::new(recept_path)
+// .has_header(true)
+// .finish()
+// .unwrap()
+// .groupby([col("customer_id")])
+// .agg([col("amount").sum().alias("amount")]);
+
+// let joined = customer_df.left_join(recept_df, col("customer_id"), col("customer_id"))
+// .select([
+//     col("*").exclude(["amount"]),
+//     col("amount").fill_null(lit(0)).alias("amount")
+// ]);
+
+// let non_zero_df = joined.clone().filter(col("amount").gt(0))
+// .collect().unwrap();
+
+// let zero_df = joined.filter(col("amount").eq(0))
+// .collect().unwrap();
+
+// println!("non-zero :{:?}", non_zero_df.shape()); //zero_dfよりすくないので、こっちに合わせる
+// println!("zero : {:?}", zero_df.shape());
+
+// let zero_df = zero_df
+//     .sample_n(non_zero_df.shape().0, false, true, None).unwrap();
+
+// println!("picked zero : {:?}", zero_df.shape());
