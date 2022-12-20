01ced59815648241414e8cb3218e2a8f50cb16a5
obutora <obutora@gmail.com>
2022-12-20 15:14:59 +0900

    add:a90

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,54 @@ use polars::prelude::*;
 fn main() {
     let recept_path = "100knocks-preprocess/docker/work/data/receipt.csv";
     // let store_path = "100knocks-preprocess/docker/work/data/store.csv";
-    let customer_path = "100knocks-preprocess/docker/work/data/customer.csv";
+    // let customer_path = "100knocks-preprocess/docker/work/data/customer.csv";
     // let product_path = "100knocks-preprocess/docker/work/data/product.csv";
     // let category_path = "100knocks-preprocess/docker/work/data/category.csv";
     // let geocode_path = "100knocks-preprocess/docker/work/data/geocode.csv";
 
-    let customer_df = LazyCsvReader::new(customer_path)
-        .has_header(true)
-        .finish()
-        .unwrap();
+    fn ymd_to_ym(ymd: &Series) -> Series {
+        ymd.i64()
+            .unwrap()
+            .into_iter()
+            .map(|ymd| match ymd {
+                Some(ymd) => (ymd as f64 / 100.0).floor() as i32,
+                None => 0i32,
+            })
+            .collect()
+    }
+
 
     let recept_df = LazyCsvReader::new(recept_path)
     .has_header(true)
     .finish()
     .unwrap()
-    .groupby([col("customer_id")])
+    .select([
+        all(),
+        col("sales_ymd").map(|s| Ok(ymd_to_ym(&s)), GetOutput::default()).alias("sales_ym")
+    ])
+    .groupby([col("sales_ym")])
     .agg([col("amount").sum().alias("amount")])
-    .filter(col("amount").gt(0));
-    
+    .sort(
+        "sales_ym",
+        SortOptions {
+            descending: (false),
+            nulls_last: (true),
+        },
+    )
+    .collect()
+    .unwrap();
 
-    let joined = customer_df.inner_join(recept_df, col("customer_id"), col("customer_id"))
-        .collect()
-        .unwrap()
-        .sample_frac(1.0, false, true, None)
-        .unwrap();
+    let train1 = recept_df.clone().slice(0, 12);
+    let val1 = recept_df.clone().slice(12, 6);
 
-    let shape = joined.shape();
-    println!("origin: {:?}", shape);
-    
-    let train = joined.slice(0, ((shape.0 as f32) * 0.8).floor() as usize);
-    let test = joined.slice(((shape.0 as f32) * 0.8).floor() as i64, ((shape.0 as f32) * 0.2).round() as usize);
+    let train2 = recept_df.slice(6, 12);
+    let val2 = recept_df.slice(18, 6);
 
-    println!("{:?}", train.shape());
-    println!("{:?}", test.shape());
+    let train3 = recept_df.slice(12, 12);
+    let val3 = recept_df.slice(24, 6);
 
-    println!("{:?}", train.tail(Some(10)));
-    println!("{:?}", test.head(Some(10)));
+    println!("{:?}", train3);
+    println!("{:?}", val3);
     
 }
 
@@ -75,3 +87,49 @@ fn main() {
 
 //     println!("{:?}", train.tail(Some(10)));
 //     println!("{:?}", test.head(Some(10)));
+
+
+//NOTE: A90
+// fn ymd_to_ym(ymd: &Series) -> Series {
+//     ymd.i64()
+//         .unwrap()
+//         .into_iter()
+//         .map(|ymd| match ymd {
+//             Some(ymd) => (ymd as f64 / 100.0).floor() as i32,
+//             None => 0i32,
+//         })
+//         .collect()
+// }
+
+
+// let recept_df = LazyCsvReader::new(recept_path)
+// .has_header(true)
+// .finish()
+// .unwrap()
+// .select([
+//     all(),
+//     col("sales_ymd").map(|s| Ok(ymd_to_ym(&s)), GetOutput::default()).alias("sales_ym")
+// ])
+// .groupby([col("sales_ym")])
+// .agg([col("amount").sum().alias("amount")])
+// .sort(
+//     "sales_ym",
+//     SortOptions {
+//         descending: (false),
+//         nulls_last: (true),
+//     },
+// )
+// .collect()
+// .unwrap();
+
+// let train1 = recept_df.clone().slice(0, 12);
+// let val1 = recept_df.clone().slice(12, 6);
+
+// let train2 = recept_df.slice(6, 12);
+// let val2 = recept_df.slice(18, 6);
+
+// let train3 = recept_df.slice(12, 12);
+// let val3 = recept_df.slice(24, 6);
+
+// println!("{:?}", train3);
+// println!("{:?}", val3);
