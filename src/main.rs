1209ec37c9ad390435bac0574fe1690d02b6faf2
obutora <obutora@gmail.com>
2022-12-20 11:28:31 +0900

    add : a82

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,77 @@
 use polars::prelude::*;
 
 fn main() {
-    // let recept_path = "100knocks-preprocess/docker/work/data/receipt.csv";
+    let recept_path = "100knocks-preprocess/docker/work/data/receipt.csv";
     // let store_path = "100knocks-preprocess/docker/work/data/store.csv";
     let customer_path = "100knocks-preprocess/docker/work/data/customer.csv";
     // let product_path = "100knocks-preprocess/docker/work/data/product.csv";
     // let category_path = "100knocks-preprocess/docker/work/data/category.csv";
-    let geocode_path = "100knocks-preprocess/docker/work/data/geocode.csv";
+    // let geocode_path = "100knocks-preprocess/docker/work/data/geocode.csv";
 
     let customer_df = LazyCsvReader::new(customer_path)
         .has_header(true)
         .finish()
         .unwrap();
-    
-    let geocode_df = LazyCsvReader::new(geocode_path)
-        .has_header(true)
-        .finish()
-        .unwrap()
-        .groupby([col("postal_cd")])
-        .agg([
-            col("longitude").mean().alias("m_lng"),
-            col("latitude").mean().alias("m_lat")
-            ]);
 
-    let joined = customer_df.inner_join(geocode_df, col("postal_cd"), col("postal_cd"))
+    let recept_df = LazyCsvReader::new(recept_path)
+    .has_header(true)
+    .finish()
+    .unwrap()
+    .groupby([col("customer_id")])
+    .agg([col("amount").sum().alias("amount")])
+    .filter(col("amount").gt(0));
+    
+
+    let joined = customer_df.inner_join(recept_df, col("customer_id"), col("customer_id"))
         .collect()
         .unwrap()
-        .head(Some(10));
+        .sample_frac(1.0, false, true, None)
+        .unwrap();
+
+    let shape = joined.shape();
+    println!("origin: {:?}", shape);
     
-    println!("{:?}", joined);
+    let train = joined.slice(0, ((shape.0 as f32) * 0.8).floor() as usize);
+    let test = joined.slice(((shape.0 as f32) * 0.8).floor() as i64, ((shape.0 as f32) * 0.2).round() as usize);
+
+    println!("{:?}", train.shape());
+    println!("{:?}", test.shape());
+
+    println!("{:?}", train.tail(Some(10)));
+    println!("{:?}", test.head(Some(10)));
+    
 }
+
+
+//NOTE: A89
+// let customer_df = LazyCsvReader::new(customer_path)
+//         .has_header(true)
+//         .finish()
+//         .unwrap();
+
+//     let recept_df = LazyCsvReader::new(recept_path)
+//     .has_header(true)
+//     .finish()
+//     .unwrap()
+//     .groupby([col("customer_id")])
+//     .agg([col("amount").sum().alias("amount")])
+//     .filter(col("amount").gt(0));
+    
+
+//     let joined = customer_df.inner_join(recept_df, col("customer_id"), col("customer_id"))
+//         .collect()
+//         .unwrap()
+//         .sample_frac(1.0, false, true, None)
+//         .unwrap();
+
+//     let shape = joined.shape();
+//     println!("origin: {:?}", shape);
+    
+//     let train = joined.slice(0, ((shape.0 as f32) * 0.8).floor() as usize);
+//     let test = joined.slice(((shape.0 as f32) * 0.8).floor() as i64, ((shape.0 as f32) * 0.2).round() as usize);
+
+//     println!("{:?}", train.shape());
+//     println!("{:?}", test.shape());
+
+//     println!("{:?}", train.tail(Some(10)));
+//     println!("{:?}", test.head(Some(10)));
